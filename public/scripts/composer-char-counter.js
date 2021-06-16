da1d2513513e60f197c6172297334bbea843c67b
hyuntony <ankim90@gmail.com>
2021-06-16 17:41:52 -0400

    create html for tweets and implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  const textarea1 = $('#tweet-text');
+  textarea1.on('input', function() {
+    const currentCount = ($(this).val().length);
+    const counter = $(this).closest('.tweet-form').find('.counter');
+    const countRemain = 140 - currentCount;
+    
+    counter[0].value = countRemain;
+    if (countRemain < 0) {
+      counter.addClass("negative");
+    }
+    if (countRemain >= 0) {
+      counter.removeClass("negative");
+    }
+  });
+});
