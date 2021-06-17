2d97aac635227a743ca5d38ab91ec7bbde134358
hyuntony <ankim90@gmail.com>
2021-06-16 20:29:51 -0400

    create ajax GET request to /tweets and append it to tweets-container utilizing renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const createTweetElement = function(tweet) {
   const $tweet = `
     <article class="tweet">
@@ -65,7 +40,12 @@ const renderTweets = function(tweets) {
 };
 
 $(document).ready(function() {
-  console.log($('.tweet-form'));
+  $.ajax('/tweets', { method: 'GET' })
+    .then(function(tweets) {
+      const $tweets = renderTweets(tweets);
+      $('#tweets-container').append($tweets);
+    });
+
   $('.tweet-form').submit(function(event) {
     const request = {
       url: '/tweets',
@@ -75,6 +55,4 @@ $(document).ready(function() {
     $.ajax(request);
     event.preventDefault();
   });
-//   const $tweets = renderTweets(data);
-//   $('#tweets-container').append($tweets);
 });
